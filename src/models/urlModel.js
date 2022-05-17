07a8f8be80bb5dce1d5f87f6ee1e3e0c5551e616
Him104 <himanshuchauhan696@gmail.com>
2022-05-17 23:05:45 +0530

    2nd commit

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -1,3 +1,25 @@
 
+const mongoose = require("mongoose")
 
-{ urlCode: { mandatory, unique, lowercase, trim }, longUrl: {mandatory, valid url}, shortUrl: {mandatory, unique} }
+const createUrl = new mongoose.Schema ({
+    
+    urlCode: { 
+    type: String,
+    required:true, 
+    unique:true,
+     lowercase:true, 
+     trim:true }, 
+     longUrl: {
+       type:String,
+       required: true, 
+       valid url}, 
+
+       shortUrl: {
+        type:String,
+        required: true, 
+        unique:true}
+     }
+    
+    {timestamps:true}
+
+    module.exports.createUrl = this.createUrl;
